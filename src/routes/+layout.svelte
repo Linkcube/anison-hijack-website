04cb8330d5aa28b48eaf0f0ae24f185f5a00788b
Linkcube <poohbah125@gmail.com>
2024-10-18 12:52:09 -0400

    Width scaling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,7 @@
         border-top-width: 10px;
         border-style: solid;
         border-bottom-width: 10px;
+        min-width: 530px;
     }
 
     .logo-link {
@@ -22,43 +23,60 @@
     }
 
     .logo-img {
-        height: 151.8px;
-        width: 216px;
-        margin-top: -50px;
+        height: calc(1518px * .2);
+        width: calc(2160px * .2);
+        margin-top: -125px;
     }
 
     .nav-links-container {
         display: flex;
         justify-content: space-around;
-        margin-left: 5%;
         width: 100%
     }
 
     .nav-link {
-        padding-left: max(10px, 2%);
-        padding-right: max(10px, 2%);
         color: white;
         font-weight: bold;
         letter-spacing: 1px;
         border-right: 3px solid white;
-        width: 100%
+        width: 100%;
+        text-align: center;
     }
 
     .nav-img-container {
         width: 50px;
+        flex: .5;
+        display: flex;
     }
 
     .nav-img {
         height: 100%;
+        width: 50px;
+    }
+
+    @media (max-width: 1100px) {
+        .logo-img {
+            height: calc(1518px * .1);
+            width: calc(2160px * .1);
+            margin-top: -50px;
+        }
+
+        .logo-link, .nav-links-container {
+            margin-left: 0;
+        }
+    }
+
+    @media (max-width: 800px) {
+        .logo-img {
+            display: none;
+        }
     }
 </style>
 
 <nav class="layout-container">
-    <!-- <div class="logo-container"> -->
-        <a class="logo-link" href="/" id="currpage">
-            <img class="logo-img" src="/logo.png">
-        </a>
-    <!-- </div> -->
+    <a class="logo-link" href="/" id="currpage">
+        <img class="logo-img" src="/logo.png">
+    </a>
     <div class="nav-links-container">
         <a class="nav-link" href="/staff">STAFF</a>
         <a class="nav-link" href="/archive">ARCHIVE</a>
@@ -66,11 +84,9 @@
         <a class="nav-link" href="https://store.anisonhijack.moe">STORE</a>
         <a class="nav-link" href="/donate">DONATE</a>
         <a class="nav-link" href="/contact">CONTACT</a>
-        <span class="nav-img-container" style="flex: 0.5; display: flex">
-            <a href="https://linktr.ee/anisonhijack" target="_blank">
-                <img class="nav-img" src="/social/generic.png" alt="Linktree">
-            </a>
-        </span>
+        <a href="https://linktr.ee/anisonhijack" target="_blank">
+            <img class="nav-img" src="/social/generic.png" alt="Linktree">
+        </a>
     </div>
 </nav>
 
